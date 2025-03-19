e037f50c8f13a4191135dbdb294f475c61eadaca
Patrick Mvula <mvlapatrick@gmail.com>
2025-03-19 12:37:57 +0200

    feat: sideabr

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem
+} from "./ui/sidebar";
+
+import { Home, Image } from "lucide-react";
+
+const items = [
+  {
+    title: "Home",
+    url: "/home",
+    icon: Home,
+  },
+  {
+    title: "Decode",
+    url: "/decode-image",
+    icon: Image,
+  },
+];
+
+export const AppSidebar = () => {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+};
